38d63c997710332ce5f77bae423956adf632db44
srot006 <girisrotoswini@gmail.com>
2026-02-17 19:51:03 +0530

    Create UC2.java

---
+++ UC2.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp
+ * Use Case 2: Print "OOPS" as 7-line ASCII Banner
+ * 
+ * @author sanchayan76
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Application entry point
+     */
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *****   *****   ***** ");
+        System.out.println("*     *  *       *       *     ");
+        System.out.println("*     *  *       *       *     ");
+        System.out.println(" *****   *       *****   *     ");
+    }
+}
